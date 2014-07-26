6cc6eeace95c515d98ebef719303d28dc0a80f05
unenglishable <unenglishable@gmail.com>
2014-07-26 03:36:09 -1000

    fix epochMap to work as an example

--- epochMap.js
+++ epochMap.js
@@ -2,7 +2,7 @@ var lolipop = require('../lolipop/lolipop');
 var config = require('./config.json');
 var lp = lolipop(config);
 var table = process.argv[2];
-var map = require(process.argv[3]); // some json file describing the map
+//var map = require(process.argv[3]); // some json file describing the map
 
 var through = require('through');
 var tr = through(write);
@@ -11,13 +11,11 @@ var tr = through(write);
 // can use a map here on some object
 // to tell how fields need to be mapped
 function write(row) {
-  var = data{};
-  console.log(data);
-  this.queue(data);
+  console.log(row);
 }
 
 var rowStream = lp.createRowStream(null, table);
 
-rowStream.pipe(userStream); // can be piped to some leveldb handler
+rowStream.pipe(tr).pipe(process.stdout); // can be piped to some leveldb handler
 
 lp.end();
