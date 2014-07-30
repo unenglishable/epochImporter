69143cc5754d13dfcc0e9cd4fe5d37fab366659f
unenglishable <unenglishable@gmail.com>
2014-07-29 21:07:51 -1000

    add epochThreadStream (similar to epochBoardStream)
    creates a stream of threadObjects to import to core

---
+++ epochThreadStream.js
@@ -0,0 +1,32 @@
+var lolipop = require('../lolipop/lolipop');
+var epochMap = require('./epochMap.js');
+var through = require('through');
+
+var EpochThreadStream = module.exports = function EpochThreadStream(lpConfig) {
+  if (!(this instanceof EpochThreadStream)) {
+    return new EpochThreadStream(lpConfig);
+  }
+  this.lp = lolipop(lpConfig);
+}
+
+EpochThreadStream.prototype.createThreadStream = function (err, oldBoardId, newBoardId) {
+  var table = 'smf_topics';
+
+  var smfMap = {
+    ID_TOPIC : 'thread_id',
+    ID_FIRST_MSG : 'post_id'
+  }
+
+  var rowStreamWhere = this.lp.createRowStreamWhere(null, table, { ID_BOARD : oldBoardId});
+  var tr = through(function (row) {
+    var obj = { board_id : newBoardId };
+    var smfObject = epochMap.remapObject(row, smfMap);
+    obj['smf'] = smfObject;
+    this.queue(obj);
+  });
+  threadStream = rowStreamWhere.pipe(tr);
+
+  this.lp.end();
+
+  return threadStream;
+}
---
+++ testEpochThreadStream.js
@@ -0,0 +1,13 @@
+var through = require('through');
+var epochThreadStream = require('./epochThreadStream');
+var lpConfig = require('./config.json');
+
+var ets = epochThreadStream(lpConfig);
+var threadStream = ets.createThreadStream(null, 1, 0);
+
+var tr = through(function (data) {
+  console.log('test: ');
+  console.log(data);
+});
+
+threadStream.pipe(tr);
