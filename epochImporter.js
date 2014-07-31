c5eaab5f12c8cc694d26a1c78cf4806ae7ce03e5
unenglishable <unenglishable@gmail.com>
2014-07-31 04:13:42 -1000

    handle err in callbacks
    needed to use the data on callback to get the new id since the data
    returned isn't just the new id data; it is the entire new object!
    
    when the code wasn't handling errors, the new object was actually null
    instead of what it should have been

--- epochImporter.js
+++ epochImporter.js
@@ -15,7 +15,11 @@ var boardStreamThrough = through(function (boardObject) {
   oldBoardId = boardObject.smf.board_id;
 
   // import to core and use callback to create a new thread stream
-  boards.import(boardObject, function (newBoardId) {
+  boards.import(boardObject, function (err, newBoard) {
+    if (err) {
+      error(err);
+    }
+    var newBoardId = newBoard.id;
     var ets = epochThreadStream(lp);
     var threadStream = ets.createThreadStream(null, oldBoardId, newBoardId);
 
@@ -23,12 +27,21 @@ var boardStreamThrough = through(function (boardObject) {
       oldThreadId = threadObject.smf.thread_id;
 
       // import to core and use callback to create a new post stream
-      posts.import(threadObject, function (newThreadId) {
+      posts.import(threadObject, function (err, newThread) {
+        if (err) {
+          error(err);
+        }
+        var newThreadId = newThread.thread_id;
         var eps = epochPostStream(lp);
         var postStream = eps.createPostStream(null, oldThreadId, newThreadId);
 
         var postStreamThrough = through(function (postObject) {
-          posts.import(postObject);
+          posts.import(postObject, function (err, newPost) {
+            if (err) {
+              error(err);
+            }
+            console.log('postId: '+newPost.smf.post_id);
+          });
         });
         postStream.pipe(postStreamThrough);
       });
