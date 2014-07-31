107e4552af1f171b223b22986c4ddf8f5910b314
unenglishable <unenglishable@gmail.com>
2014-07-30 23:52:47 -1000

    update epochImporter to use boards, threads, and posts from core
    these are currently broken due to high amount of connections.

--- epochImporter.js
+++ epochImporter.js
@@ -1,17 +1,19 @@
 var through = require('through');
 var epochBoardStream = require('./epochBoardStream');
 var epochThreadStream = require('./epochThreadStream');
+var epochPostStream = require('./epochPostStream');
+var boards = require('../core/boards.js');
+var posts = require('../core/posts.js');
 var lpConfig = require('./config.json');
 
 var ebs = epochBoardStream(lpConfig);
 var boardStream = ebs.createBoardStream(null);
 
 var boardStreamThrough = through(function (boardObject) {
-  /*
   oldBoardId = boardObject.smf.board_id;
 
   // import to core and use callback to create a new thread stream
-  importBoardMethodWithCallback(data, function (newBoardId) {
+  boards.import(boardObject, function (newBoardId) {
     var ets = epochThreadStream(lpConfig);
     var threadStream = ets.createThreadStream(null, oldBoardId, newBoardId);
 
@@ -19,23 +21,20 @@ var boardStreamThrough = through(function (boardObject) {
       oldThreadId = threadObject.smf.thread_id;
 
       // import to core and use callback to create a new post stream
-      importThreadMethodWithCallback(data, function (newThreadId) {
+      posts.import(threadObject, function (newThreadId) {
         var eps = epochPostStream(lpConfig);
         var postStream = eps.createPostStream(null, oldThreadId, newThreadId);
 
         var postStreamThrough = through(function (postObject) {
-          importPostMethodWithCallback(data, function (newPostId) {
-            // do nothing? lol.
-          });
+          posts.import(postObject);
         });
         postStream.pipe(postStreamThrough);
       });
+      console.log('test: ');
+      console.log(threadObject);
     });
     threadStream.pipe(threadStreamThrough);
   });
-  */
-  console.log('test: ');
-  console.log(boardObject);
 });
 
 boardStream.pipe(boardStreamThrough);
