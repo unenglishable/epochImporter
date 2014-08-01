1a9072c7e19caa4e90b147136357fc39e10fa165
unenglishable <unenglishable@gmail.com>
2014-07-31 18:12:19 -1000

    update boards to use through2

--- testDrivers/testEpochBoardStream.js
+++ testDrivers/testEpochBoardStream.js
@@ -1,4 +1,4 @@
-var through = require('through');
+var through = require('through2');
 var epochBoardStream = require('../epochBoardStream');
 var lolipop = require('../../lolipop/lolipop');
 var lpConfig = require('../config.json');
@@ -7,9 +7,14 @@ var lp = lolipop(lpConfig);
 var ebs = epochBoardStream(lp);
 var boardStream = ebs.createBoardStream();
 
-var tr = through(function (data) {
+var tr = through({ objectMode : true}, function (data, enc, cb) {
   console.log('test: ');
   console.log(data);
+  this.push(data);
+  cb();
+},
+function() {
+  lp.end();
 });
 
 boardStream.pipe(tr);
