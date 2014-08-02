5e4e65d202bc03535189781c343150e99d9da78d
unenglishable <unenglishable@gmail.com>
2014-08-01 18:02:46 -1000

    epochImperter through2 implementation

--- epochImporter.js
+++ epochImporter.js
@@ -1,4 +1,4 @@
-var through = require('through');
+var through2 = require('through2');
 var epochBoardStream = require('./epochBoardStream');
 var epochThreadStream = require('./epochThreadStream');
 var epochPostStream = require('./epochPostStream');
@@ -11,43 +11,48 @@ var lp = lolipop(lpConfig);
 var ebs = epochBoardStream(lp);
 var boardStream = ebs.createBoardStream(null);
 
-var boardStreamThrough = through(function (boardObject) {
+boardStream.pipe(through2.obj(function (boardObject, enc, trBoardCb) {
   // import to core and use callback to create a new thread stream
   boards.import(boardObject, function (err, newBoard) {
     if (err) {
       error(err);
     }
+
+    trBoardCb();
+
     var oldBoardId = newBoard.smf.board_id;
     console.log('boardId: '+oldBoardId);
     var newBoardId = newBoard.id;
     var ets = epochThreadStream(lp);
     var threadStream = ets.createThreadStream(null, oldBoardId, newBoardId);
 
-    var threadStreamThrough = through(function (threadObject) {
+    threadStream.pipe(through2.obj(function (threadObject, enc, trThreadCb) {
       // import to core and use callback to create a new post stream
       posts.import(threadObject, function (err, newThread) {
         if (err) {
           error(err);
         }
+
+        trThreadCb();
+
         var oldThreadId = newThread.smf.thread_id;
         console.log('threadId: '+oldThreadId);
         var newThreadId = newThread.thread_id;
         var eps = epochPostStream(lp);
         var postStream = eps.createPostStream(null, oldThreadId, newThreadId);
 
-        var postStreamThrough = through(function (postObject) {
+        postStream.pipe(through2.obj(function (postObject, enc, trPostCb) {
           posts.import(postObject, function (err, newPost) {
             if (err) {
               error(err);
             }
+
+            trPostCb();
+
             console.log('postId: '+newPost.smf.post_id);
           });
-        });
-        postStream.pipe(postStreamThrough);
+        }));
       });
-    });
-    threadStream.pipe(threadStreamThrough);
+    }));
   });
-});
-
-boardStream.pipe(boardStreamThrough);
+}));
