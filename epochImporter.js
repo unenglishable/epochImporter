7ac20601605205e5c4d279d95048ae1a62c1b234
unenglishable <unenglishable@gmail.com>
2014-08-01 18:29:27 -1000

    epochImporter now uses async

--- epochImporter.js
+++ epochImporter.js
@@ -8,51 +8,71 @@ var lolipop = require('../lolipop/lolipop');
 var lpConfig = require('./config.json');
 var lp = lolipop(lpConfig);
 
-var ebs = epochBoardStream(lp);
-var boardStream = ebs.createBoardStream(null);
+var async = require('async');
+var concurrency = Number.MAX_VALUE; // Concurrency handled by lolipop
 
-boardStream.pipe(through2.obj(function (boardObject, enc, trBoardCb) {
-  // import to core and use callback to create a new thread stream
-  boards.import(boardObject, function (err, newBoard) {
-    if (err) {
-      error(err);
-    }
+var asyncQueue = async.queue(function (runTask, callback) {
+  runTask(callback);
+}, concurrency);
 
-    trBoardCb();
+asyncQueue.drain = function () {
+  lp.end(function () {
+    console.log('Import complete.');
+  });
+}
 
-    var oldBoardId = newBoard.smf.board_id;
-    console.log('boardId: '+oldBoardId);
-    var newBoardId = newBoard.id;
-    var ets = epochThreadStream(lp);
-    var threadStream = ets.createThreadStream(null, oldBoardId, newBoardId);
+asyncQueue.push(function (asyncBoardCb) {
 
-    threadStream.pipe(through2.obj(function (threadObject, enc, trThreadCb) {
-      // import to core and use callback to create a new post stream
-      posts.import(threadObject, function (err, newThread) {
-        if (err) {
-          error(err);
-        }
+  var ebs = epochBoardStream(lp);
+  var boardStream = ebs.createBoardStream(null);
 
-        trThreadCb();
+  boardStream.pipe(through2.obj(function (boardObject, enc, trBoardCb) {
+    boards.import(boardObject, function (err, newBoard) {
+      if (err) {
+        error(err);
+      }
 
-        var oldThreadId = newThread.smf.thread_id;
-        console.log('threadId: '+oldThreadId);
-        var newThreadId = newThread.thread_id;
-        var eps = epochPostStream(lp);
-        var postStream = eps.createPostStream(null, oldThreadId, newThreadId);
+      trBoardCb();  // Don't return.  Async will handle end.
 
-        postStream.pipe(through2.obj(function (postObject, enc, trPostCb) {
-          posts.import(postObject, function (err, newPost) {
+      asyncQueue.push(function (asyncThreadCb) {
+
+        var oldBoardId = newBoard.smf.board_id;
+        console.log('boardId: '+oldBoardId);
+        var newBoardId = newBoard.id;
+        var ets = epochThreadStream(lp);
+        var threadStream = ets.createThreadStream(null, oldBoardId, newBoardId);
+
+        threadStream.pipe(through2.obj(function (threadObject, enc, trThreadCb) {
+          posts.import(threadObject, function (err, newThread) {
             if (err) {
               error(err);
             }
 
-            trPostCb();
+            trThreadCb();  // Don't return.  Async will handle end.
 
-            console.log('postId: '+newPost.smf.post_id);
+            asyncQueue.push(function (asyncPostCb) {
+
+              var oldThreadId = newThread.smf.thread_id;
+              console.log('threadId: '+oldThreadId);
+              var newThreadId = newThread.thread_id;
+              var eps = epochPostStream(lp);
+              var postStream = eps.createPostStream(null, oldThreadId, newThreadId);
+
+              postStream.pipe(through2.obj(function (postObject, enc, trPostCb) {
+                posts.import(postObject, function (err, newPost) {
+                  if (err) {
+                    error(err);
+                  }
+
+                  trPostCb();  // Don't return.  Async will handle end.
+
+                  console.log('postId: '+newPost.smf.post_id);
+                });
+              }, asyncPostCb));  // When stream is empty, worker is done
+            });
           });
-        }));
+        }, asyncThreadCb));  // When stream is empty, worker is done
       });
-    }));
-  });
-}));
+    });
+  }, asyncBoardCb));  // When stream is empty, worker is done
+});
